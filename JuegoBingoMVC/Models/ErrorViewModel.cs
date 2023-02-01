4c41989b01f36c2a7ab5a978cfc9a91fcfbf87ea
MatiasMolina000 <molina_matias@mi.unc.edu.ar>
2023-02-01 13:27:22 -0300

    Primer commit de mi solución web del clásico juego de Bingo

---
+++ JuegoBingoMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JuegoBingoMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
