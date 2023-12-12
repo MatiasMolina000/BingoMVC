6a80bc25f5acf7f24661a25967a5f9c2658b26b4
Matias Molina <matias_molina@mi.unc.edu.ar>
2023-12-12 14:15:06 -0300

    First commit, Bingo API Rest solution

--- JuegoBingoAPI/Models/ResponseModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JuegoBingoAPI.Models
-{
-    public class ResponseModel
-    {
-        public bool Status { get; set; }
-        public string? Message { get; set; }
-        public string? Data { get; set; }
-    }
-}
