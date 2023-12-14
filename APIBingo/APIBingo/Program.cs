eaf9bd1904607d7892441accf2f5d45b3e848a3a
Matias Molina <matias_molina@mi.unc.edu.ar>
2023-12-14 20:59:53 -0300

    Modify add token configuration in swagger

--- APIBingo/APIBingo/Program.cs
+++ APIBingo/APIBingo/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,37 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// o Start SWAGGER. =================================================================================>>>
+builder.Services.AddSwaggerGen(sg => {
+    // Swagger -> JWT section.
+    sg.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authotization",
+        Type = SecuritySchemeType.Http,//ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Here Enter JWT with bearer format lie Bearer {token}"
+    });
+    // Agrega el esquema de seguridad Bearer a nivel global
+    sg.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[]{ }
+        }
+    });
+
+});
+// _ End SWAGGER.
+
 
 var app = builder.Build();
 
