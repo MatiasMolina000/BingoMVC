148e88947bccbd00ab848331cd1a8685377bbfdf
Matias Molina <matias_molina@mi.unc.edu.ar>
2023-12-14 21:43:51 -0300

    Add sql query, DDL and DML.

---
+++ docs/redesign/BingoGameDML.sql
@@ -0,0 +1,2 @@
+INSERT INTO dbo.UStatus ([Status]) VALUES ('Activo'),('Inactivo'),('Blanqueo')
+INSERT INTO dbo.Users ([User], StatusId, Email, [Password], [PassTemp]) VALUES ('Admin', 1, 'admin@gmail.com','admin123','')
