c51833f43f3f3be849c958c1e3d39cef10d1ec0a
Molina Matias Ezequiel <96317744+MatiasMolina000@users.noreply.github.com>
2024-01-09 17:27:32 -0300

    Create BingoGameSeedTest.sql

---
+++ docs/BingoGameSeedTest.sql
@@ -0,0 +1,5 @@
+INSERT INTO Users ([User], Email, [Password], PassTemp) VALUES ('admin','admin@gmail.com','admin123','')
+
+INSERT INTO UStatus ([Status]) VALUES ('Active'), ('Blocked'), ('InActive')
+
+INSERT INTO GStatus ([Status]) VALUES ('Created'), ('Finished'), ('Closed')
