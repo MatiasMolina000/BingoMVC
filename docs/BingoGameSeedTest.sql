c56201b17391e84e0195c61fa8e9d2b1fa1fa6ec
Matias Molina <matias_molina@mi.unc.edu.ar>
2024-01-28 18:39:55 -0300

    First commit, front_dev branch

--- docs/BingoGameSeedTest.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Users ([User], Email, [Password], PassTemp) VALUES ('admin','admin@gmail.com','admin123','')
-
-INSERT INTO UStatus ([Status]) VALUES ('Active'), ('Blocked'), ('InActive')
-
-INSERT INTO GStatus ([Status]) VALUES ('Created'), ('Finished'), ('Closed')
