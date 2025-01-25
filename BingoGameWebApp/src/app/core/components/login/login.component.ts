f950b75fea2a7f85f0ecb144a986aed9f497901f
Matias Molina <matias_molina@mi.unc.edu.ar>
2025-01-25 14:48:21 -0300

    Add validators and error messages in login

--- BingoGameWebApp/src/app/core/components/login/login.component.ts
+++ BingoGameWebApp/src/app/core/components/login/login.component.ts
@@ -2,7 +2,9 @@ import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+
 import { ILogin } from '../../../shared/models/login.interface';
+import { LoginService } from '../../../shared/services/api/login.service';
 
 @Component({
   selector: 'app-login',
@@ -10,29 +12,64 @@ import { ILogin } from '../../../shared/models/login.interface';
   styleUrl: './login.component.sass'
 })
 export class LoginComponent {
-  form: FormGroup;
+  private loginService = inject(LoginService);
   private router = inject(Router);
-
-  public credentialError: boolean = false;
+  
+  credentialError: boolean = false;
+  errorMessage: string = '';
+  form: FormGroup;
+  private minLengthInput: number = 1;
+  private maxLengthInput: number = 255;
 
   constructor() {
     this.form = new FormGroup({
-      email: new FormControl('',[
-        Validators.required
+      email: new FormControl('', [
+        Validators.required,
+        Validators.minLength(this.minLengthInput),
+        Validators.maxLength(this.maxLengthInput),
+        Validators.email
       ]),
-      password: new FormControl('',[
-        Validators.required
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(this.minLengthInput),
+        Validators.maxLength(this.maxLengthInput)
       ])
-    })
+    });
   }
 
   onSubmit() {
-    const loginData: ILogin = this.form.value;
-    if (loginData.email == 'admin' && loginData.password == 'admin') {
-      this.router.navigate(['menu']);
-    } else {
-      this.credentialError = true;
+    this.credentialError = false;
+    if (this.form.valid) {
+      const loginData: ILogin = this.form.value;
+      this.loginService.signIn(loginData).subscribe({
+        next: (response: string) => { 
+          this.router.navigate(['menu']);
+        },
+        error: (error) => {
+          this.credentialError = true;
+          this.errorMessage = error;
+          console.error(error);
+        }
+      });
     }
+  }
 
+  buildErrorMessageForm(controlName: string): string {
+    const control = this.form.controls[controlName];
+    if (control.touched && control.invalid) {
+      if (control.hasError('required')) {
+        return `${controlName.charAt(0).toUpperCase() + controlName.slice(1)} is required.`;
+      }
+      if (control.hasError('minlength')) {
+        return `${controlName.charAt(0).toUpperCase() + controlName.slice(1)} must be at least ${this.minLengthInput} character long.`;
+      }
+      if (control.hasError('maxlength')) {
+        return `${controlName.charAt(0).toUpperCase() + controlName.slice(1)} cannot be longer than ${this.maxLengthInput} characters.`;
+      }
+      if (control.hasError('email')) {
+        return `Please enter a valid email address.`;
+      }
+    }
+    return '';
   }
 }
